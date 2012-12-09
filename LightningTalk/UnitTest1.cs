a7dcbf43cef2e8799d0e8b9738adc5aea8569057
Steven Suing <Steven.suing@gmail.com>
2012-12-09 04:48:20 -0800

    FIrst

---
+++ LightningTalk/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+
+namespace LightningTalk
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://www.reddit.com/r/bbq");
+         
+        }
+    }
+}
